8fcdc1b9d79946c8ad6678c6a04e1682208b14e9
Bruno de Souza <109471488+bsantuz@users.noreply.github.com>
2022-09-20 20:29:06 -0300

    update designer and add get Readme more easy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,102 @@
-import ReadmeEdit from './components/ReadmeEdit'
+import readmeEdit from './components/ReadmeEdit';
+import './style/style.css'
+import { Fragment } from 'react';
 
 function App() {
-  return (
-    <div className="App container">
-      <ReadmeEdit/>
-    </div>
-  );
+
+  const readme = new readmeEdit()
+
+  function createInput(father, feature = false){
+    let f = document.getElementById(father);
+
+    let s = document.createElement("span")
+        s.innerText = f.getElementsByTagName("input").length + " - ";
+    let i = document.createElement("input");
+        if(feature){ i.value = "- [ ] "} else {i.value = "* "}
+    let b = document.createElement("br");
+
+    f.appendChild(s)
+    f.appendChild(i)
+    f.appendChild(b)
+  }
+
+
+  return(
+    <Fragment>
+      <div className="container-sm mt-5">
+
+          <label htmlFor="title">Titulo:<span>(Obrigatório)</span></label>
+          <input id="title" type="text" />
+          
+
+          <label htmlFor="description">Breve descrição:<span>(Obrigatório)</span></label>
+          
+          <textarea id="description" cols="60" rows="5"></textarea>
+          
+
+          <label htmlFor="status">Status dos projeto:<span>(Obrigatório)</span></label>
+          <input id ="status" type="text" />
+          
+
+          <label htmlFor="features">Features</label>
+          <button onClick={()=>{createInput("features", true)}}>+</button>
+          
+          <div id="features"></div>
+          
+
+          <label htmlFor="demostração">Demonstração da Aplicação:<span>(Obrigatório)</span></label>
+          
+          <textarea 
+            id="demostração" 
+            cols="60" 
+            rows="5"
+            defaultValue={readme.demonstrationDefault}
+          ></textarea>
+          
+
+          <label htmlFor="requisitos">Pré-requisitos e como rodar a aplicação/testes:<span>(Obrigatório)</span></label>
+          
+          <textarea 
+          id="requisitos" 
+          cols="60" rows="5"
+          defaultValue={readme.requirementsDefault}
+          ></textarea>
+          
+          
+          <label htmlFor="tecnologias">Tecnologias utilizadas:<span>(Obrigatório)</span></label>
+          <button onClick={()=>{createInput("tecnologias")}}>+</button>
+          
+          <div id="tecnologias"></div>
+          
+
+          <label htmlFor="autor">Autor:<span>(Obrigatório)</span></label>
+          <input id="autor" type="text" />
+          <label htmlFor="autor">Email Contato:<span>(Obrigatório)</span></label>
+          <input id="email" type="text" />
+          
+
+          <label htmlFor="licença">Licença:<span>(Obrigatório)</span></label>
+          <input id="licença" type="text" defaultValue="MIT License"/>
+          
+          <button onClick={
+            readme.stringifild
+          }>submit</button>
+      </div>
+      <div id="modal" style={{display:"none"}}>
+        <div>
+          <textarea id="readme"></textarea>
+          <div style={{display:"flex", width:"100%", flexDirection:"row-reverse"}}>
+            <button
+              onClick={()=>{
+                document.querySelector("#modal").style.display = "none";
+              }}
+              >back</button>
+          </div>
+          
+        </div>
+      </div>
+    </Fragment>
+)
 }
 
 export default App;
--- src/components/ReadmeEdit.js
+++ src/components/ReadmeEdit.js
@@ -1,42 +1,57 @@
-import React from "react"
 
+export default class readmeEdit{
 
-function createInput(father, feature = false){
-    let f = document.getElementById(father);
+    demonstrationDefault =(
+`1 - ...
+2 - ...
+3 - ...
+<a href="https://...">Teste a aplicação<a>
+<img src="./...web.png">
+<img src="./...mob.png" width="300">`
+    )
+   
+    requirementsDefault=(
+`Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas:
 
-    let s = document.createElement("span")
-        s.innerText = f.getElementsByTagName("input").length + " - ";
-    let i = document.createElement("input");
-        if(feature){ i.value = "- [ ] "} else {i.value = "* "}
-    let b = document.createElement("br");
+Além disto é bom ter um editor para trabalhar com o código como [VSCode](https://code.visualstudio.com/)
 
-    f.appendChild(s)
-    f.appendChild(i)
-    f.appendChild(b)
-}
+${"```bash"}
+# Clone este repositório
+$ git clone <>
 
-function getValue(el){
-    return document.getElementById(el).value
-}
+# Acesse a pasta do projeto no terminal/cmd
+$ cd ...
 
-function arrayForString(el){
-    let f = document.getElementById(el).getElementsByTagName("input")
-    let a = ''
-    if(f.length > 0){
-        for (let index = 0; index < f.length; index++) { a +="\n"+ f[index].value }
-    }
-    return a
+# Instale as dependências
+$ npm install ...
 
-}
+# Execute a aplicação 
+$ npm start
 
-function stringifild(){
+# inciará na porta:3333 - acesse <http://localhost:3333>
+${"```"}`)
+
+    stringifild(){
+  
+        function getValue(el){
+            return document.getElementById(el).value
+        }
+      
+       function arrayForString(el){
+        let f = document.getElementById(el).getElementsByTagName("input")
+        let a = ''
+        if(f.length > 0){
+            for (let index = 0; index < f.length; index++) { a +="\n"+ f[index].value }
+        }
+        return a
+      
+        }
 
     let feature = arrayForString("features")
     let tecnologias = arrayForString("tecnologias")
-
+      
     let readme= (
-        `
-# ${getValue("title")}
+`# ${getValue("title")}
 <p align="center">${getValue("description")}</p>
 <h4 align="center"> Status do projeto: ${getValue("status")}</h4>
 
@@ -48,7 +63,7 @@ function stringifild(){
 <a href="#Como-executar-o-projeto">como Executar</a> • 
 <a href="#Tecnologias-utilizadas">Tecnologias</a> •   
 <a href="#autor">Autor</a> •
-<a href="#licenc-a">Licença</a> 
+<a href="#licença">Licença</a> 
 </p>
 
 ### funcionalidades
@@ -69,87 +84,11 @@ Feito com ❤️ por ${getValue("autor")} 👋🏽 Entre em contato!
 
 ### Licença
 ${getValue("licença")}
-`
-    )
+`)
+    document.querySelector("#modal").style.display = "flex";
+    document.querySelector("#readme").innerHTML = readme;
+
     console.log(readme)
-}
-
-export default function readmeEdit(){
-
-    return(
-        <div className="container my-5">
-
-            <label htmlFor="title">Titulo:<strong>(Obrigatório)</strong></label>
-            <input id="title" type="text" />
-            <br/>
-
-            <label htmlFor="description">Breve descrição:<strong>(Obrigatório)</strong></label>
-            <br/>
-            <textarea id="description" cols="60" rows="5"></textarea>
-            <br/>
-
-            <label htmlFor="status">Status dos projeto:<strong>(Obrigatório)</strong></label>
-            <input id ="status" type="text" />
-            <br/>
-
-            <label htmlFor="features">Features</label>
-            <button onClick={()=>{createInput("features", true)}}>+</button>
-            <br/>
-            <div id="features"></div>
-            <br/>
-
-            <label htmlFor="demostração">Demonstração da Aplicação:<strong>(Obrigatório)</strong></label>
-            <br/>
-            <textarea id="demostração" cols="60" rows="5"></textarea>
-            <br/>
-
-            <label htmlFor="requisitos">Pré-requisitos e como rodar a aplicação/testes:<strong>(Obrigatório)</strong></label>
-            <br/>
-            <textarea 
-            id="requisitos" 
-            cols="60" rows="5"
-            defaultValue={`
-Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas:
-
-Além disto é bom ter um editor para trabalhar com o código como [VSCode](https://code.visualstudio.com/)
-
-${"```bash"}
-# Clone este repositório
-$ git clone <>
-
-# Acesse a pasta do projeto no terminal/cmd
-$ cd ...
-
-# Instale as dependências
-$ npm install ...
-
-# Execute a aplicação 
-$ npm start
-
-# inciará na porta:3333 - acesse <http://localhost:3333>
-${"```"}
-            `}
-            ></textarea>
-            <br/>
-            
-            <label htmlFor="tecnologias">Tecnologias utilizadas:<strong>(Obrigatório)</strong></label>
-            <button onClick={()=>{createInput("tecnologias")}}>+</button>
-            <br/>
-            <div id="tecnologias"></div>
-            <br/>
-
-            <label htmlFor="autor">Autor:<strong>(Obrigatório)</strong></label>
-            <input id="autor" type="text" />
-            <label htmlFor="autor">Email Contato:<strong>(Obrigatório)</strong></label>
-            <input id="email" type="text" />
-            <br/>
-
-            <label htmlFor="licença">Licença:<strong>(Obrigatório)</strong></label>
-            <input id="licença" type="text" defaultValue="MIT License"/>
-            <br/>
-            <button onClick={()=>{
-                stringifild()
-            }}>submit</button>
-        </div>
-    )
+  }
+      
 }
